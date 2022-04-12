e8855bea2e2809cf2fa08cfcbbc59ec504dcecb6
Tihomir Pavlovic <tihomir.pavlovic1112@gmail.com>
2022-04-12 17:05:27 +0200

    contact, single blog, blog, seervices desktop

--- External JS/global.js
+++ External JS/global.js
@@ -24,15 +24,42 @@ $(document).ready(function () {
     $(".footer_disclosure").slideToggle();
   });
 
+  $(".filter_button").on("click", function () {
+    var currentPlan = $(this).data("plan");
+    $(".filter_button").removeClass("active");
+    $(this).addClass("active");
+
+    $(".four_box_section").each(function () {
+      if ($(this).data("plan") == currentPlan) {
+        $(this).css("display", "flex").addClass("in_view");
+      } else {
+        $(this).css("display", "none");
+      }
+    });
+  });
+
   $(".single_team_member").on("click", function () {
     $(this).toggleClass("active");
-    $(this).find('.team_description').slideToggle();
+    $(this).find(".team_description").slideToggle();
+  });
+
+  $(".total_posts span").text($(".posts_slider .single_slide").length);
+
+  $(".single_featured_blog").hover(function () {
+    let currentImage = $(this).data("blog");
+
+    $(".blog_hero_bottom_content .image_holder img").each(function () {
+      if ($(this).data("blog") == currentImage) {
+        $(".blog_hero_bottom_content .image_holder img").removeClass("active");
+        $(this).addClass("active");
+      }
+    });
   });
 
   $(".mobile_menu_opener").on("click", function () {
     $(this).toggleClass("opened");
-	$('body').toggleClass("no_scroll");
-	$('header').toggleClass("opened_menu_mobile");
+    $("body").toggleClass("no_scroll");
+    $("header").toggleClass("opened_menu_mobile");
     $(".mobile_nav").fadeToggle();
   });
 
@@ -52,6 +79,24 @@ $(document).ready(function () {
     }
   });
 
+  $(".partners_slider").slick({
+    slidesToShow: 5,
+    infinite: true,
+    dots: false,
+    prevArrow: ".prev_button",
+    nextArrow: ".next_button",
+  });
+
+  $(".posts_slider").slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    rows: 3,
+    infinite: true,
+    dots: true,
+    prevArrow: ".prev_post_button",
+    nextArrow: ".next_post_button",
+  });
+
   function sliderInit() {
     var $slider = $(".hub_slider");
     $slider.each(function () {
@@ -62,18 +107,17 @@ $(document).ready(function () {
         infinite: true,
         prevArrow: ".prevButton",
         nextArrow: ".nextButton",
-		responsive: [
-			
-			{
-			  breakpoint: 750,
-			  settings: {
-				slidesToShow: 1.2
-			  }
-			}
-			// You can unslick at a given breakpoint now by adding:
-			// settings: "unslick"
-			// instead of a settings object
-		]
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              slidesToShow: 1.2,
+            },
+          },
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ],
       });
 
       if ($(this).find(".item").length > 1) {
